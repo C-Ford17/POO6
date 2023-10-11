d3ecb640ef7668bd460cfc7a31a411336b344a5b
Christian <cagomezb@unimagdalena.edu.co>
2023-10-10 22:21:23 -0500

    r2

--- src/Elector.java
+++ src/Elector.java
@@ -3,16 +3,6 @@ import java.time.LocalDate;
 public record Elector(int dni, String nombre, LocalDate fechaNacimiento) implements Cloneable {
 
     @Override
-    public String toString() {
-        return "Elector{" +
-                "dni=" + dni +
-                ", nombre='" + nombre + '\'' +
-                ", fechaNacimiento=" + fechaNacimiento +
-                '}';
-    }
-
-
-    @Override
     public Elector clone() throws CloneNotSupportedException {
         return (Elector) super.clone();
     }
